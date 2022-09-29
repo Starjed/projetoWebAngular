cee65e6a9a0be0c872ab912ab42702fdc53b2de7
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2022-09-29 13:24:57 -0400

    Exibição do calculo das datas, sendo necessário apenas a aplicação da máscara agora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalculaDataComponent} from "./calcula-data/calcula-data.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calculaData', component: CalculaDataComponent},
+  { path: '',   redirectTo: '/calculaData', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {TextMaskModule} from "angular2-text-mask";
+import { CalculaDataComponent } from './calcula-data/calcula-data.component';
+import {CommonModule, DatePipe} from "@angular/common";
+import { registerLocaleData } from '@angular/common';
+import { WjInputModule } from '@grapecity/wijmo.angular2.input';
+
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr)
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculaDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    TextMaskModule,
+    ReactiveFormsModule,
+    CommonModule,
+    DatePipe,
+    WjInputModule
+
+
   ],
-  providers: [],
+  providers: [DatePipe, {provide: LOCALE_ID, useValue: 'pt-PT' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
